254d61d2017de1c45399ec55dd0e4e8d554f863f
Pavlos Daniel <pavlos.daniel@acquia.com>
2023-10-17 22:37:59 +0000

    Initial commit.

---
+++ src/Commands/BltPhpcsPrecommitHook.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Acquia\BltPhpcsPrecommitHook\Commands;
+
+use Acquia\Blt\Robo\BltTasks;
+use Acquia\Blt\Robo\Exceptions\BltException;
+
+class BltPhpcsPrecommitHook extends BltTasks {
+
+  /**
+   * This will be called after the pre-commit command.
+   *
+   * @hook pre-commit
+   */
+  public static function preCommit($changed_files): void {
+    try {
+      $this->invokeCommand('validate:phpcs');
+    } catch (BltException $e) {
+      $this->yell('Your code has failed pre-commit validation.', 40, 'red');
+      return 1;
+    }
+
+    $this->say("<info>Your local code has passed git pre-push validation.</info>");
+  }
+
+}
